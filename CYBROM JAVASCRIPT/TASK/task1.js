06180fb3bb4ce730613807568ea3cf49ec7e6b1c
Ramroop26 <ramroop26io071@satiengg.in>
2025-05-29 17:50:37 +0530

    commit

--- CYBROM JAVASCRIPT/TASK/task1.js
+++ CYBROM JAVASCRIPT/TASK/task1.js
@@ -2,23 +2,23 @@
 
 //-----------------------------------------------FILPCART TASK -----------------------------------------------------//
 
-// let image1 = ()=>{
-//     let show = document.querySelector("#main")
-//     show.style.backgroundImage = 'URL(https://tse3.mm.bing.net/th?id=OIP.U_VJuupQohwnzXcKMztqWgHaEo&pid=Api&P=0&h=180)'
-// }
+let image1 = ()=>{
+    let show = document.querySelector("#main")
+    show.style.backgroundImage = 'URL(https://tse3.mm.bing.net/th?id=OIP.U_VJuupQohwnzXcKMztqWgHaEo&pid=Api&P=0&h=180)'
+}
 
 
-// let image2 = ()=>{
-//     let show1 = document.querySelector("#main")
-//     show1.style.backgroundImage = 'URL(https://tse3.mm.bing.net/th?id=OIP.j4KdqaXpnhbN94WzVyHUhAHaE8&pid=Api&P=0&h=180)'
-// }
+let image2 = ()=>{
+    let show1 = document.querySelector("#main")
+    show1.style.backgroundImage = 'URL(https://tse3.mm.bing.net/th?id=OIP.j4KdqaXpnhbN94WzVyHUhAHaE8&pid=Api&P=0&h=180)'
+}
 
 
 
-// let image3 = ()=>{
-//     let show2 = document.querySelector("#main")
-//     show2.style.backgroundImage = 'URL(https://themewagon.github.io/Coffo/images/img-1.png)'
-// }
+let image3 = ()=>{
+    let show2 = document.querySelector("#main")
+    show2.style.backgroundImage = 'URL(https://themewagon.github.io/Coffo/images/img-1.png)'
+}
 
 
 // -------------------------------------------PARAGRAPH FONT SIZE TASK---------------------------------------------------------//
